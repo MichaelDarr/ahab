8f5ee8d67024d3ad6418bb3c30d98b27b2154763
Michael Darr <michael.e.darr@gmail.com>
2020-02-10 00:46:32 -0800

    Generalize most docker commands
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,10 @@ import (
 	"github.com/MichaelDarr/docker-config/internal"
 )
 
-var verbose bool
+// Execute is the entrypoint to the dcfg CLI
+func Execute() {
+	rootCmd.Execute()
+}
 
 var rootCmd = &cobra.Command{
 	Use:     "dcon",
@@ -15,7 +18,41 @@ var rootCmd = &cobra.Command{
 	Version: internal.Version,
 }
 
-// Execute is the entrypoint to the dcfg CLI
-func Execute() {
-	rootCmd.Execute()
+// verbose is used as a flag for various commands
+var verbose bool
+
+// Docker commands that don't take options
+var diffCmd = BasicCommand("diff", "Inspect changes to files or directories on container filesystem")
+var pauseCmd = BasicCommand("pause", "Pause all processes within container")
+var unpauseCmd = BasicCommand("unpause", "Unpause all processes within container")
+
+// Docker commands that take options
+var attachCmd = OptionCommand("attach", "Attach local standard input, output, and error streams to container")
+var commitCmd = OptionCommand("commit", "Create a new image from container's changes")
+var exportCmd = OptionCommand("export", "Export container’s filesystem as a tar archive")
+var killCmd = OptionCommand("kill", "Kill container")
+var rmCmd = OptionCommand("rm", "Remove container")
+var startCmd = OptionCommand("start", "Start stopped container")
+var stopCmd = OptionCommand("stop", "Stop running container")
+
+// Shell attachment commands
+var bashCmd = ShellCommand("bash", "bash")
+var shCmd = ShellCommand("sh", "bourne")
+var zshCmd = ShellCommand("zsh", "z")
+
+// init adds all the generic subcommands
+func init() {
+	rootCmd.AddCommand(&attachCmd)
+	rootCmd.AddCommand(&bashCmd)
+	rootCmd.AddCommand(&commitCmd)
+	rootCmd.AddCommand(&diffCmd)
+	rootCmd.AddCommand(&exportCmd)
+	rootCmd.AddCommand(&killCmd)
+	rootCmd.AddCommand(&pauseCmd)
+	rootCmd.AddCommand(&rmCmd)
+	rootCmd.AddCommand(&shCmd)
+	rootCmd.AddCommand(&startCmd)
+	rootCmd.AddCommand(&stopCmd)
+	rootCmd.AddCommand(&unpauseCmd)
+	rootCmd.AddCommand(&zshCmd)
 }
---
+++ cmd/templates.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/MichaelDarr/docker-config/internal"
+)
+
+// BasicCommand constructs and returns a Docker container command which doesn not take extra options
+func BasicCommand(command string, description string) cobra.Command {
+	return cobra.Command{
+		Use:   command,
+		Short: description,
+		Long: description + `
+
+Docker Command:
+  docker ` + command + ` [CONFIG_OPTIONS]
+	`,
+		Run: func(cmd *cobra.Command, args []string) {
+			config, configPath, err := internal.ProjectConfig()
+			internal.PrintErrFatal(err)
+
+			err = internal.DockerContainerCmd(config, configPath, command, nil)
+			internal.PrintErrFatal(err)
+		},
+	}
+}
+
+// OptionCommand constructs and returns a Docker container command which takes extra options
+func OptionCommand(command string, description string) cobra.Command {
+	return cobra.Command{
+		Use:   command,
+		Short: description,
+		Run: func(cmd *cobra.Command, args []string) {
+			helpRequested, err := internal.PrintDockerHelp(&args, "kill", description+`
+
+Docker Command:
+  docker `+command+` [OPTIONS] CONTAINER
+
+Usage:
+  `+internal.CmdName+` `+command+` [-h/--help] [OPTIONS]
+`)
+			internal.PrintErrFatal(err)
+			if helpRequested {
+				return
+			}
+			config, configPath, err := internal.ProjectConfig()
+			internal.PrintErrFatal(err)
+
+			err = internal.DockerContainerCmd(config, configPath, command, &args)
+			internal.PrintErrFatal(err)
+		},
+		Args:               cobra.ArbitraryArgs,
+		DisableFlagParsing: true,
+	}
+}
+
+// ShellCommand constructs and returns a Docker container command to attach a shell to the active terminal
+func ShellCommand(shellCommand string, description string) cobra.Command {
+	return cobra.Command{
+		Use:   shellCommand,
+		Short: "Open a containerized " + description + " shell",
+		Long: `Attach a containerized ` + description + ` shell to the active terminal.
+
+*Warning!* the ` + description + ` shell must be installed in your image for this command to function!
+
+Docker Command:
+  docker exec -it CONTAINER ` + shellCommand,
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			config, configPath, err := internal.ProjectConfig()
+			internal.PrintErrFatal(err)
+
+			err = internal.UpContainer(config, configPath)
+			internal.PrintErrFatal(err)
+
+			execArgs := []string{"exec", "-it", internal.ContainerName(config, configPath), shellCommand}
+			err = internal.DockerCmd(&execArgs)
+			internal.PrintErrFatal(err)
+		},
+	}
+}
