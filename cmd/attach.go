ac725be64076dda1598cf87e38de2aaa5786111b
Michael Darr <michael.e.darr@gmail.com>
2020-02-09 23:06:19 -0800

    Add attach command
    Signed-off-by: Michael Darr <michael.e.darr@gmail.com>

---
+++ cmd/attach.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/MichaelDarr/docker-config/internal"
+)
+
+var attachCmd = &cobra.Command{
+	Use:   "attach",
+	Short: "Attach local standard input, output, and error streams to container",
+	Args:  cobra.ArbitraryArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		helpRequested, err := internal.PrintDockerHelp(&args, "attach", `Attach local standard input, output, and error streams to container
+
+Docker Command:
+  docker attach [OPTIONS] CONTAINER
+
+Usage:
+  `+internal.CmdName+` attach [-h/--help] [OPTIONS]
+`)
+		internal.PrintErrFatal(err)
+		if helpRequested {
+			return
+		}
+		config, configPath, err := internal.ProjectConfig()
+		internal.PrintErrFatal(err)
+
+		err = internal.UpContainer(config, configPath)
+		internal.PrintErrFatal(err)
+
+		err = internal.DockerContainerCmd(config, configPath, "attach", &args)
+		internal.PrintErrFatal(err)
+	},
+	DisableFlagParsing: true,
+}
+
+func init() {
+	rootCmd.AddCommand(attachCmd)
+}
